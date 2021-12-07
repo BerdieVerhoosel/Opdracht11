e665324d7cf3398bc4b6dc7a58cf44695ee7e82c
BerdieVerhoosel <91963456+BerdieVerhoosel@users.noreply.github.com>
2021-12-07 19:01:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hello Winc Academy students");
+console.log("This is a large sum");
+let multiply = 1230941 * 1823794;
+console.log("Multiply:", multiply);
+let divide = 637263 / 54;
+console.log("Divide:", divide);
